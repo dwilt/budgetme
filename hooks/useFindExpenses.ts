0ae5c44892539bd392e3967aff8a70c276c2e2d3
Dan Wilt <dwilt4rville@gmail.com>
2022-09-18 15:57:39 -0400

    Update useFindExpenses.ts

--- hooks/useFindExpenses.ts
+++ hooks/useFindExpenses.ts
@@ -5,9 +5,26 @@ import { Expense } from '../api/expense/types'
 export const useFindExpenses = (
   { account_id, ...rest }: Partial<FindExpensesParams>,
   options: Omit<
-    UseQueryOptions<Expense[]>,
+    UseQueryOptions<
+      Expense[] | undefined,
+      unknown,
+      Expense[] | undefined,
+      (
+        | string
+        | {
+            id?: number | undefined
+            name?: string | undefined
+            transaction_date?: string | undefined
+            amount?: number | undefined
+            recurring?: boolean | undefined
+            account_id: number | undefined
+          }
+      )[]
+    >,
     'queryKey' | 'queryFn' | 'initialData'
-  >
+  > & {
+    initialData?: () => undefined
+  }
 ) => {
   return useQuery(
     ['expenses', { account_id, ...rest }],
